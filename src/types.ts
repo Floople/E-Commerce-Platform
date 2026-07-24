99b7c65edffc528beb91900e6ad2fe4cc6847c5a
Floople <michaelzhangflys@gmail.com>
2026-07-24 13:21:28 -0400

    initial check in

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export interface Address {
+    line1: string;
+    line2: string;
+    city: string;
+    postalCode: string;
+    state: string;
+    country: string;
+}
+
+export interface Customer {
+    id: string;
+    name: string;
+    billingAddress: Address;
+    shippingAddress: Address;
+    email: string;
+    createdAt: number;
+    lastModifiedAt: number;
+}
+
+export interface Product {
+    id: string;
+    sku: string;
+    name: string;
+    description: string;
+    price: number;
+    createdAt: number;
+    lastModifiedAt: number;
+}
